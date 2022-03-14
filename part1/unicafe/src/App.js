92f9728b640b794e45130a07a772647477323a8f
rmolu <richardmbouombouo@gmail.com>
2022-03-14 00:29:15 -0500

    unicafe step2 complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 const Button = ({ onClick, title }) => <> <button onClick={onClick}>{title}</button> </>
 
-const Statistic = ({ title, total }) => <p>{title}: {total}</p>
+const Statistic = ({ title, count }) => <p><b>{title}:</b> {count}</p>
 
 
 const App = () => {
@@ -13,17 +13,22 @@ const App = () => {
   const goodOne = () => setGood(good + 1)
   const neutralOne = () => setNeutral(neutral + 1)
   const badOne = () => setBad(bad + 1)
-
+  const total = good + neutral + bad
+  const average =(1 * good + 0 * neutral + -1 * bad) / total
+  const positive = good/total * 100 + " %"
   return (
     <div>
-      <h1>Feedback Form </h1>
+      <h1>Feedback Form</h1>
       <Button onClick={goodOne} title="good" />
       <Button onClick={neutralOne} title="neutral" />
       <Button onClick={badOne} title="bad" />
       <h1>Statistics </h1>
-      <Statistic title="Good" total={good} />
-      <Statistic title="Neutral" total={neutral} />
-      <Statistic title="Bad" total={bad} />
+      <Statistic title="Good" count={good} />
+      <Statistic title="Neutral" count={neutral} />
+      <Statistic title="Bad" count={bad} />
+      <Statistic title="All" count={total} />
+      <Statistic title="Average" count={average} />
+      <Statistic title="Positive" count={positive} />
     </div>
   )
 }
